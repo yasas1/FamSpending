57f64086bfad74a5ca7e794e75d5db7fef25432f
yasas1 <32437531+yasas1@users.noreply.github.com>
2021-01-19 12:26:52 +0530

    home calendar

--- FamSpending/src/app/home/home.module.ts
+++ FamSpending/src/app/home/home.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { NgCalendarModule   } from 'ionic2-calendar';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgCalendarModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
--- FamSpending/src/app/home/home.page.ts
+++ FamSpending/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild , Inject, LOCALE_ID} from '@angular/core';
+import { formatDate } from '@angular/common';
+
+import { AlertController } from '@ionic/angular';
+import { CalendarComponent } from 'ionic2-calendar';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,95 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  eventSource = [];
+  viewTitle: string;
+
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+    dateFormatter: {
+      formatMonthViewDay: function(date:Date) {
+          return date.getDate().toString();
+      }            
+  }
+  };
+ 
+  selectedDate: Date;
+ 
+  @ViewChild(CalendarComponent) myCal: CalendarComponent;
+
+
+  constructor(
+    private alertCtrl: AlertController,
+    @Inject(LOCALE_ID) private locale: string,
+    // private database: DatabaseService, 
+    // private alertViewer: ViewControlerService
+    ) {}
+
+  
+    next() {
+      this.myCal.slideNext();
+    }
+   
+    back() {
+      this.myCal.slidePrev();
+    }
+   
+    // Selected date reange and hence title changed
+    onViewTitleChanged(title) {
+      this.viewTitle = title;
+    }
+   
+    // Calendar event was clicked
+    async onEventSelected(event) {
+      // Use Angular date pipe for conversion
+      let start = formatDate(event.startTime, 'fullDate', this.locale);
+      let end = formatDate(event.endTime, 'fullDate', this.locale);
+   
+      const alert = await this.alertCtrl.create({
+        header: start,
+        subHeader: event.desc,
+        message: 'Total Spending: ' + 'Rs.1500 ',
+        buttons: ['OK'],
+      });
+      alert.present();
+    }
+  
+    onCurrentDateChanged(event:Date){
+      console.log(event);
+    }
+  
+    markDisabled = (date: Date) => {
+      var current = new Date();
+      return date >= current;
+    };
+  
+  addExpendituresDB() {
+
+
+    this.createRandomEvents();
+      
+  }
+
+  createRandomEvents() {
+    var events = [];
+    var date = new Date("2021-01-20");
+
+    console.log( Date.UTC(2021,0,15));
+    events.push({
+      title: 'Total Spending Rs.1500',
+      startTime: new Date(Date.UTC(2021,0,20)),
+      endTime: new Date(Date.UTC(2021,0,20)),
+      allDay: false,
+    },
+    {
+      title: 'Total Spending Rs.700',
+      startTime: new Date(Date.UTC(2021,0,18)),
+      endTime: new Date(Date.UTC(2021,0,18)),
+      allDay: false,
+    });
+   
+    this.eventSource = events;
+  }
 
 }
